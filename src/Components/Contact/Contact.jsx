3d9bb021427e27b3dfc148d1f21fb1cb204924b1
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-07-12 13:22:42 -0400

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
 
           <div className='flex flex-row items-center gap-8 justify-center pt-2 z-50 '>
             <a href='https://www.instagram.com/juaanmoree/'><BsInstagram className=' pt-2 w-11 h-10 text-transparent text-purple-500' style={{ color: '#9F7AEA' }}/></a>
-            <a href='https://wa.me/message/HN32S5RMOTEBI1'><BsWhatsapp className=' pt-2 w-11 h-10 text-transparent  text-green-500'/></a>
+            <a href='https://wa.me/message/HN32S5RMOTEBI1'><BsWhatsapp className=' pt-2 w-11 h-10 text-transparent  text-green-500'style={{ color: '#22C55E' }}/></a>
           </div>
         </div>
       </div>
@@ -34,8 +34,8 @@ const Contact = () => {
           <p className='text-[#f3f3f7] md:items-center md:justify-center text-md font-semibold '>Agustín Casal </p>
           <p className='text-[#c4c4f1] md:items-center md:justify-center text-sm font-normal '>agustin_casal@outlook.com</p>
           <div className='flex flex-row items-center gap-8 justify-center pt-2 z-50'>
-            <a href='https://www.instagram.com/aguscasall/'><BsInstagram className=' pt-2 w-11 h-10 text-transparent text-purple-500 bg'/></a>
-            <a href='https://wa.me/message/HN32S5RMOTEBI1'><BsWhatsapp className=' pt-2 w-11 h-10 text-transparent  text-green-500'/></a>
+            <a href='https://www.instagram.com/aguscasall/'><BsInstagram className=' pt-2 w-11 h-10 text-transparent text-purple-500 bg'style={{ color: '#9F7AEA' }}/></a>
+            <a href='https://wa.me/message/HN32S5RMOTEBI1'><BsWhatsapp className=' pt-2 w-11 h-10 text-transparent  text-green-500'style={{ color: '#22C55E' }}/></a>
           </div>
         </div>
       </div>
