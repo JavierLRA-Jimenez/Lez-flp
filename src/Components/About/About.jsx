667d36ea0e41564bac6afe0900071ebecdbea4d7
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-07-24 23:54:14 -0400

    Cambios de arquitectura header

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,44 +1,41 @@
-import React from 'react'
-import {BsPersonFill} from 'react-icons/bs'
-
-//Image
-import infierno from '../../assets/maskgrislez.webp'
+import React from 'react';
+import { BsPersonFill } from 'react-icons/bs';
+import infierno from '../../assets/maskgrislez.webp';
 
 const About = () => {
   return (
     <section className='md:h-[37rem]' id='about'>
-      <div className='h-full bg-cover bg-center'style={{ backgroundImage: `url(${infierno})` }}  >
+      <div className='h-full bg-cover bg-center' style={{ backgroundImage: `url(${infierno})` }}>
 
-        <h1 className='text-7xl font-bold p-8 items-center justify-center flex gap-8 text-white'><BsPersonFill className='bg-blue-800 w-9 h-9  p-2 rounded-full box-content'/>About Me</h1>
+        <h1 className='text-7xl font-bold p-8 items-center justify-center flex gap-8 text-white'><BsPersonFill className='bg-blue-800 w-9 h-9 p-2 rounded-full box-content' />About Me</h1>
 
-        <div className='flex flex-row justify-between gap-3 text-white '>
+        <div className='flex flex-col md:flex-row justify-between gap-3 text-white'>
 
-          <div className='bg-[#1d1d64d5] rounded-xl p-5 m-5 opacity-80 leading-7 md:text-l md:items-center md:justify-center'>
+          <div className='bg-[#1d1d64d5] rounded-xl p-5 m-2 opacity-80 text-xs md:text-lg'>
             <p>Lez is originally from Rosario, Santa Fe, Argentina. With a career that began in 2013 and
                having been considered a promise in Latin America, Lez has shown himself to be a versatile
                DJ and producer capable of adapting to different styles and trends, quickly standing out for
                his talent and charisma behind the decks.At the same time, he studied at the Cetear
                Institute, which allowed him to expand his knowledge and skills in music production. </p>
-            <br/>
+            <br />
             <p>
-               His talent and skills have allowed him to perform at different clubs and events in the nightlife
-               scene of Rosario, such as Coyote Iglú, Barracao, Complejo Awas, Costello, Madame,
-               Metropolitano Rosario, among others. Throughout his career, he has shared the stage with
-               great artists from the electronic music scene, including Ice Machine, Gastón Garay, Nacho
-               Scoppa, Zone7, Cadelago, among others.</p>
-            <br/>
+              His talent and skills have allowed him to perform at different clubs and events in the nightlife
+              scene of Rosario, such as Coyote Iglú, Barracao, Complejo Awas, Costello, Madame,
+              Metropolitano Rosario, among others. Throughout his career, he has shared the stage with
+              great artists from the electronic music scene, including Ice Machine, Gastón Garay, Nacho
+              Scoppa, Zone7, Cadelago, among others.</p>
+            <br />
             <p>Currently, Lez is focused on his own music productions and artistic growth. His productions
                have led him to collaborate with other artists such as Jean Bacarreza (known today as
                Bakka), Zone7, SUPER (ARG), and other outstanding talents from the electronic music
                scene. In addition, he works together with LEVELS, his team that guides him in his career
                and is part of his artist booking.</p>
-
           </div>
-  
+
         </div>
       </div>
-   </section>
+    </section>
   )
 }
 
-export default About
+export default About;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,34 @@
-import React, {useState} from 'react'
-import whitelogo from '../../assets/LEZCIR.png'
-
-
-//Iconos 
-import {RiMenuUnfoldFill} from 'react-icons/ri'
-import {MdOutlineCancel} from 'react-icons/md'
+import React, { useState } from 'react';
+import whitelogo from '../../assets/LEZCIR.png';
+import { RiMenuUnfoldFill } from 'react-icons/ri';
+import { MdOutlineCancel } from 'react-icons/md';
 
 const Header = () => {
-    const [showMenu, setShowMenu ] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Función para cerrar el menú automáticamente después de hacer clic en un enlace
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
     <header className='flex items-center justify-between xl:justify-start w-full bg-[#191919] p-2 h-[10vh]'>
-      <div className=' h-[60px] w-[90px] m-4 p-1'>
-        <img src={whitelogo} alt="logo lez" className='w-[70%] flex justify-center m-auto relative'/>
+      <div className='h-[60px] w-[90px] m-4 p-1'>
+        <img src={whitelogo} alt="logo lez" className='w-[70%] flex justify-center m-auto relative' />
       </div>
 
-      <nav className={`fixed bg-[#191919] w-[80%] md:w-[40%]  xl:w-full h-full ${showMenu ? 'left-0' : '-left-full'} top-0 xl:static flex-1 flex text-white justify-center items-center font-bold gap-10 flex-col xl:flex-row transition-all duration-700 z-50`}>   
-        <a href='#body'  className=''>Home </a>
-        <a href='#about'  className=''>About </a>
-        <a href='#spotify'  className=''>Spotify </a>
-        <a href='#youtube'  className=''>Youtube </a>
-        <a href='#socialnetwork'  className=''>Network </a>
-        <a href='#contact'  className=''>Contact </a>
+      <nav className={`fixed bg-[#191919] w-[80%] md:w-[40%]  xl:w-full h-full ${showMenu ? 'left-0' : '-left-full'} top-0 xl:static flex-1 flex text-white justify-center items-center font-bold gap-10 flex-col xl:flex-row transition-all duration-700 z-50`}>
+        <a href='#body' onClick={closeMenu} className=''>Home</a>
+        <a href='#about' onClick={closeMenu} className=''>About</a>
+        <a href='#spotify' onClick={closeMenu} className=''>Spotify</a>
+        <a href='#youtube' onClick={closeMenu} className=''>Youtube</a>
+        <a href='#socialnetwork' onClick={closeMenu} className=''>Network</a>
+        <a href='#contact' onClick={closeMenu} className=''>Contact</a>
       </nav>
-      <button onClick={() => setShowMenu(!showMenu)} className='text-white xl:hidden text-2xl '>{showMenu ? <MdOutlineCancel/> : <RiMenuUnfoldFill/>}</button> 
-      
-    </header>
-  ) 
-}
+      <button onClick={() => setShowMenu(!showMenu)} className='text-white xl:hidden text-2xl '>{showMenu ? <MdOutlineCancel /> : <RiMenuUnfoldFill />}</button>
 
-export default Header
+    </header>
+  );
+};
+
+export default Header;
