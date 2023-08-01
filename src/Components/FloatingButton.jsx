3055416ecb2962e9abf6136339a61d0d1c085a2a
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-08-01 18:05:15 -0400

    Update FloatingButton.jsx

--- src/Components/FloatingButton.jsx
+++ src/Components/FloatingButton.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { RxTextAlignTop } from 'react-icons/rx';
+import { GiOrbDirection } from 'react-icons/gi';
 
 const FloatingButton = () => {
   const handleScrollToTop = () => {
@@ -9,10 +9,10 @@ const FloatingButton = () => {
 
   return (
     <div
-      className="fixed bottom-4 flex right-4 text-white items-center justify-center md:text-3xl text-xl bg-red-700 w-10 md:w-[4rem] h-10 md:h-[4rem]  rounded-full cursor-pointer z-40"
+      className="fixed bottom-4 flex right-4 text-white items-center justify-center md:text-5xl text-3xl bg-red-700 w-10 md:w-[4rem] h-10 md:h-[4rem]  rounded-full cursor-pointer z-40"
       onClick={handleScrollToTop}
     >
-      <RxTextAlignTop />
+      <GiOrbDirection />
     </div>
   );
 };
