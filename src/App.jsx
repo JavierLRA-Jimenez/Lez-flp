53c113e6cecb36989b26275e18c2d14e33815084
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-10-18 20:05:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from './Components/Container';
-import Fear from './Components/Fear/Fear'
+import Fear from './Components/Fear/Fear';
 
 const App = () => {
   return (
-    <div>
-      <Container />
-      <Fear/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Container} /> 
+        <Route path="/fear" component={Fear} />
+      </Switch>
+    </Router>
   );
 };
 
