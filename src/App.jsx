4e2416a36a211661c4d3e0ceceab0f83923e034f
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-10-18 22:13:41 -0300

    react6

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from './Components/Container';
 import Fear from './Components/Fear/Fear';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Container} /> 
-        <Route path="/fear" component={Fear} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Container />} />
+        <Route path="/fear" element={<Fear />} />
+      </Routes>
     </Router>
   );
 };
