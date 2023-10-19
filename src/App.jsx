6a1d40b63344ec3bfb660bc3c18531989be702a3
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-10-19 00:59:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from './Components/Container';
 import Fear from './Components/Fear/Fear';
 
