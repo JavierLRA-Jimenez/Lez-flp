47681795631b77277a19ed4097a901796baefbe4
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-10-18 23:30:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from './Components/Container';
 import Fear from './Components/Fear/Fear';
 
