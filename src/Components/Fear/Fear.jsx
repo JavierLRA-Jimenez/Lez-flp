7174c126fc5f4ba8549569c7e361e4122bf83826
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-11-15 19:22:25 -0300

    Update Fear.jsx

--- src/Components/Fear/Fear.jsx
+++ src/Components/Fear/Fear.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
+import video from '../../assets/Fear.mp4'
 
 const Fear = () => {
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-      <video
-        autoPlay
-        loop
-        muted
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-        }}
-      >
-        <source src="src\assets\Fear.mp4" type="video/mp4" />
-      </video>
+    <div>
+      <video src={video} type="video/mp4" controls autoplay />
     </div>
   );
 }
