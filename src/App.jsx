ba70cae7f23bd4079b74ab8c37cd746170bf1549
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-11-15 03:34:32 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './Components/Container';
-import Fear from './Components/Fear/Fear';
+import Header from './Components/Header/Header';
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Container />}></Route> 
-        <Route path="/Fear" element={<Fear/>}></Route> 
+        <Route path="/Header" element={<Header/>}></Route> 
       </Routes>
     </Router>
   );
