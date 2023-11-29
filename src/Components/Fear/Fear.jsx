51ba256fe11aba515c9cd720c4159622e65e4bfb
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-11-29 02:02:55 -0300

    Update Fear.jsx

--- src/Components/Fear/Fear.jsx
+++ src/Components/Fear/Fear.jsx
@@ -3,10 +3,21 @@ import video from '../../assets/Fear.mp4'
 
 const Fear = () => {
   return (
-    <div>
-      <video autoPlay muted loop controls>
-  <source src={video} type="video/mp4" />
-</video>
+    <div className="relative h-screen">
+      <div className="absolute inset-0 z-0">
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full h-full object-cover"
+          src={video}
+          style={{ pointerEvents: 'none' }}
+        />
+      </div>
+      <div className="relative z-10 flex justify-center items-center h-full">
+        {/* Contenido de tu sección */}
+        <h1 className="text-white text-4xl font-bold">Contenido de tu sección</h1>
+      </div>
     </div>
   );
 }
