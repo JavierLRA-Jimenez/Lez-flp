3e280d852c1a356f89860a17a208c0224baf21e7
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-12-26 17:01:44 -0300

    no config

--- src/Components/Fear/Fear.jsx
+++ src/Components/Fear/Fear.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import video from '../../assets/Fear.mp4';
-import "../Fear/Fear.css" // Asegúrate de tener tu archivo CSS para Fear
+import video from '../../assets/Fear.mp4'
 
 const Fear = () => {
   return (
@@ -8,15 +7,14 @@ const Fear = () => {
       <div className="absolute inset-0 z-0">
         <video
           autoPlay
-          muted
           loop
-          className="w-full h-full object-cover mobile-zoom"
+          className="md:w-full md:h-full object-cover flex"
           src={video}
           style={{ pointerEvents: 'none' }}
         />
       </div>
     </div>
   );
-};
+}
 
 export default Fear;
